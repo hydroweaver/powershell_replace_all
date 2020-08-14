4ed97f09f234c56a43d70ddd159d2ce5ad11bbef
Karan <hydroweaver@gmail.com>
2020-08-14 13:26:30 +0530

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,24 @@
+$excel = New-Object -Com Excel.Application
+$wb = $excel.Workbooks.Open("C:\Users\hydro\Desktop\langs.xlsx")
+$sh = $wb.Sheets.Item(1)
+$languages = 7
+$translations = 105
+$englishColumn = $sh.UsedRange.Columns(1).value2
+
+for ($j = 2; $j -le $languages; $j++)
+{	
+	$file = Get-Content .\Settings-EN.xml -Raw
+	for ($i = 2; $i -le $translations ; $i++)
+	{
+
+		$currentLanguage = $sh.UsedRange.Columns($j).value2
+		$strMsg = "`r`n"
+		$strMsg
+		$englishColumn[$i, 1], $currentLanguage[$i,1]
+		$file = ($file -replace $englishColumn[$i, 1], $currentLanguage[$i,1])
+	}
+	$fileName = $currentLanguage[1,1]
+	Set-Content $file -Encoding UTF8 -Path .\$fileName.xml
+		
+
+}
