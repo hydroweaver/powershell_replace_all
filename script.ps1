a7574e91bcc9659c3e06f409b6f63d2b08d39e99
Karan <hydroweaver@gmail.com>
2020-08-18 21:08:47 +0530

    Changed for XML tag

--- script.ps1
+++ script.ps1
@@ -1,5 +1,7 @@
+# Powershell displays Field Name="Title" Value="New to Unilever" Type="Text" as Field Name="Title" Type="Text" Value="New to Unilever", i.e. re-arranges Value and Text, so be cognizant
+
 $excel = New-Object -Com Excel.Application
-$wb = $excel.Workbooks.Open("C:\Users\hydro\Desktop\langs.xlsx")
+$wb = $excel.Workbooks.Open("C:\Users\hydro\Desktop\New Folder\langs.xlsx")
 $sh = $wb.Sheets.Item(1)
 $languages = 7
 $translations = 105
@@ -10,15 +12,13 @@ for ($j = 2; $j -le $languages; $j++)
 	$file = Get-Content .\Settings-EN.xml -Raw
 	for ($i = 2; $i -le $translations ; $i++)
 	{
-
 		$currentLanguage = $sh.UsedRange.Columns($j).value2
 		$strMsg = "`r`n"
-		$strMsg
-		$englishColumn[$i, 1], $currentLanguage[$i,1]
+		#$strMsg
+		#$englishColumn[$i, 1], $currentLanguage[$i,1]
 		$file = ($file -replace $englishColumn[$i, 1], $currentLanguage[$i,1])
 	}
+	$currentLanguage[1,1]
 	$fileName = $currentLanguage[1,1]
 	Set-Content $file -Encoding UTF8 -Path .\$fileName.xml
-		
-
 }
